be57f9d2d6124e18fb472ae4a01106078704095e
Scott Harris <scott.harris3@comcast.net>
2025-05-23 11:58:29 -0700

    Updated WebSec

--- fleet/src/main/java/wsf/example/config/SecurityConfig.java
+++ fleet/src/main/java/wsf/example/config/SecurityConfig.java
@@ -3,7 +3,25 @@ package wsf.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz -> authz
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+}
+/* 
 
 @Configuration
 public class SecurityConfig {
@@ -11,12 +29,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-        .csrf(csrf -> csrf.disable()) // <--- This should be replaced for production
-          .authorizeHttpRequests(authz -> authz
+       .csrf(csrf -> csrf.disable()) // <--- This should be replaced for production
+         .authorizeHttpRequests(authz -> authz
               .requestMatchers("/", "/public/**").permitAll()
               .anyRequest().authenticated()
           )
           .oauth2Login(); // Enable OAuth2 login
         return http.build();
-    }
-}
+   }
+   //  @Bean
+   // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+   //     http
+    //        .authorizeHttpRequests(auth -> auth
+    //            .anyRequest().authenticated()
+    //        )
+    //        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    //    return http.build();
+   // }
+} */
