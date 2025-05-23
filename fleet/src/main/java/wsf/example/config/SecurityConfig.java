fc92aeafd4d066a8a99d1fcbedcf1d8d170430a6
Scott Harris <scott.harris3@comcast.net>
2025-05-23 15:28:03 -0700

    Updated

--- fleet/src/main/java/wsf/example/config/SecurityConfig.java
+++ fleet/src/main/java/wsf/example/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-
+/*
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -22,8 +22,7 @@ public class SecurityConfig {
         return http.build();
     }
 }
-/* 
-
+     */
 @Configuration
 public class SecurityConfig {
 
@@ -47,4 +46,4 @@ public class SecurityConfig {
     //        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     //    return http.build();
    // }
-} */
+} 
